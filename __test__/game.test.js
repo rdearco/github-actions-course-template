8b0d46415d30abae05e1fa23ed532706b203d5cc
Rudolph De Arco <rudolph_de_arco@hotmail.com>
2023-07-21 11:17:08 -0700

    Update game.test.js

--- __test__/game.test.js
+++ __test__/game.test.js
@@ -1,17 +1,15 @@
 const Game = require('../src/game').default
 // const fs = require('fs')
 
-describe('App', () => {
-  it('Contains the compiled JavaScript', async (done) => {
+// describe('App', () => {
+//   it('Contains the compiled JavaScript', async (done) => {
     // fs.readFile('./public/main.js', 'utf8', (err, data) => {
     //   expect(err).toBe(null)
     //   expect(data).toMatchSnapshot()
     //   done()
     // })
-    expect(true).toBe(true);
-    done()
-  })
-})
+//   })
+// })
 
 describe('Game', () => {
   let game, p1, p2
